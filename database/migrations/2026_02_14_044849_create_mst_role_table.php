f586f3dd2c2fe0ec7245ee083064d7a5c1c1d6fd
arifianilhamnrr <244110601111@mhs.uinsaizu.ac.id>
2026-02-14 11:57:29 +0700

    Setup master schema & migrations

---
+++ database/migrations/2026_02_14_044849_create_mst_role_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mst_role', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('role_code', 5);
+            $table->string('role_name', 30);
+            $table->enum('is_active', ['0', '1']);
+            $table->dateTime('created_at')->nullable();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mst_role');
+    }
+};
