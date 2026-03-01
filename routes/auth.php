72dcfe7e456d211076dc843043a71e5507b6d966
genzabis <syahputranabil521@gmail.com>
2026-03-01 22:02:29 +0700

    update

--- routes/auth.php
+++ routes/auth.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
@@ -12,31 +11,22 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    // Disabled: Using custom login modal on landing page instead
-    // Route::get('register', [RegisteredUserController::class, 'create'])
-    //     ->name('register');
-    //
-    // Route::post('register', [RegisteredUserController::class, 'store']);
-    //
-    // Route::get('login', [AuthenticatedSessionController::class, 'create'])
-    //     ->name('login');
-    //
-    // Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    // Register
+    Route::get('register', [RegisteredUserController::class, 'create'])
+        ->name('register');
 
+    Route::post('register', [RegisteredUserController::class, 'store']);
+
+    // Forgot Password
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
         ->name('password.email');
-
-    Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-        ->name('password.reset');
-
-    Route::post('reset-password', [NewPasswordController::class, 'store'])
-        ->name('password.store');
 });
 
 Route::middleware('auth')->group(function () {
+    // Verifikasi Email
     Route::get('verify-email', EmailVerificationPromptController::class)
         ->name('verification.notice');
 
@@ -48,13 +38,16 @@ Route::middleware('auth')->group(function () {
         ->middleware('throttle:6,1')
         ->name('verification.send');
 
+    // Confirm Password
     Route::get('confirm-password', [ConfirmablePasswordController::class, 'show'])
         ->name('password.confirm');
 
     Route::post('confirm-password', [ConfirmablePasswordController::class, 'store']);
 
+    // Update Password
     Route::put('password', [PasswordController::class, 'update'])->name('password.update');
 
+    // Logout
     Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
         ->name('logout');
 });
